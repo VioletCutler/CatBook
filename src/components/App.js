0a5f80cd02c3d31d94b1273f593ebe68d4bded33
Violet Cutler <violetcutler@Violets-MacBook-Air.local>
2022-07-15 21:41:58 -0400

    cleaned up code

--- src/components/App.js
+++ src/components/App.js
@@ -6,34 +6,31 @@ import Cats from "./Cats";
 import Cat from "./Cat";
 import CatApi from "./CatApi";
 import ErrorMessage from "./ErrorMessage";
-import style from "../style/style.css";
 import catList from "../data";
 
 const App = () => {
   const [currentCatList, updateCatList] = useState(catList);
-  const [apiCatList, setApiCatList] = useState([])
+  const [apiCatList, setApiCatList] = useState([]);
 
   useEffect(() => {
     const getCats = async () => {
-        try {
-          const response = await fetch(
-            "https://api.thecatapi.com/v1/breeds", {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    "x-api-key": "407340d1-c60c-47b0-bff0-bfc2d7caeb15"
-                },
-            }
-          );
-          const cats = await response.json()
-          setApiCatList(cats)
-          console.log(Array.isArray(cats))
-        } catch (error) {
-          console.log(error);
-        }
-      };
-      getCats()
-  }, [])
+      try {
+        const response = await fetch("https://api.thecatapi.com/v1/breeds", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key": "407340d1-c60c-47b0-bff0-bfc2d7caeb15",
+          },
+        });
+        const cats = await response.json();
+        setApiCatList(cats);
+        console.log(Array.isArray(cats));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCats();
+  }, []);
 
   return (
     <>
@@ -53,7 +50,7 @@ const App = () => {
             />
           }
         />
-        <Route path="/api" element={<CatApi apiCatList={apiCatList}/>} />
+        <Route path="/api" element={<CatApi apiCatList={apiCatList} />} />
         <Route path="*" element={<ErrorMessage />} />
       </Routes>
     </>
